cf5dffa098d61798327ce0880fc566c56eecd855
Jorge A Manrique <jamanrique@gmail.com>
2022-07-30 11:58:40 -0500

    apply opacity hoever article

--- src/components/NewsCard.js
+++ src/components/NewsCard.js
@@ -35,7 +35,6 @@ const NewsCard = ({ article }) => {
     // Identify the article is in my favs:
     useEffect(() => {
         const news = localStorage.getItem("liked-news")
-        // console.log(`article.story_title: ${article.story_title}, article.story_url: ${article.story_url}, article.objectID: ${article.objectID}`);
         if(news !== null){
             const articleSearch = JSON.parse(news).filter(story => story.objectID === article.objectID);
 
@@ -77,7 +76,7 @@ const NewsCard = ({ article }) => {
                         <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 16 16">
                             <path fill="#606060" d="M8 1.333c3.676 0 6.667 2.991 6.667 6.667 0 3.676-2.991 6.667-6.667 6.667-3.676 0-6.667-2.991-6.667-6.667 0-3.676 2.991-6.667 6.667-6.667zM8 0C3.582 0 0 3.582 0 8s3.582 8 8 8 8-3.582 8-8-3.582-8-8-8zm.667 8V4H7.333v5.333H12V8H8.667z"/>
                         </svg>
-                        {timeSince(new Date(article.created_at))} ago by {article.author}
+                        <div>{timeSince(new Date(article.created_at))} ago by {article.author}</div>
                     </span>
                     <span className="article-title">
                         {article.story_title}
